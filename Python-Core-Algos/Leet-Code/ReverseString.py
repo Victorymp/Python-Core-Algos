9fa2da1892062efa31972f4f433e3ccae80488b6
Victory Mpokosa <mpokosa5@gmail.com>
2026-09-21 20:47:59 +0100

    Update reverseString to use len 1

--- Python-Core-Algos/Leet-Code/ReverseString.py
+++ Python-Core-Algos/Leet-Code/ReverseString.py
@@ -6,12 +6,14 @@ class Solution:
     Do not return anything, modify s in-place instead.
     """
     left, right = 0, len(s) - 1
-
-    while left < right:
-      print(f"---- {right}: -- {s[right]} to {left}")
+    if len(s) == 1:
       s[left], s[right] = s[right], s[left]
-      right -= 1
-      left += 1
+    if len(s) > 1:
+      while left < right:
+        print(f"---- {right}: -- {s[right]} to {left}")
+        s[left], s[right] = s[right], s[left]
+        right -= 1
+        left += 1
 
 if __name__ == "__main__":
     s = ["h","e","l","l","o"]
